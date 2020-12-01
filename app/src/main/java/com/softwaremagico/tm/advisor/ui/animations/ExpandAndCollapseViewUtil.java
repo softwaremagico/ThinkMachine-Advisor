7cc8ea559b40c177cc06ed4b8f5b8539ff7b06ee
Software Mágico <softwaremagico@gmail.com>
2020-12-01 11:55:45 +0100

    Added counter component

--- app/src/main/java/com/softwaremagico/tm/advisor/ui/animations/ExpandAndCollapseViewUtil.java
+++ app/src/main/java/com/softwaremagico/tm/advisor/ui/animations/ExpandAndCollapseViewUtil.java
@@ -10,7 +10,7 @@
  *  You should have received a copy of the GNU General Public License along with this Program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
  */
 
-package com.softwaremagico.tm.advisor.ui.load;
+package com.softwaremagico.tm.advisor.ui.animations;
 
 import android.view.View;
 import android.view.ViewGroup;
---
+++ app/src/main/java/com/softwaremagico/tm/advisor/ui/components/Counter.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (C) 2020 Softwaremagico
+ *
+ *  This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero  <softwaremagico@gmail.com> Valencia (Spain).
+ *
+ *  This program is free software; you can redistribute it and/or modify it under  the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this Program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ */
+
+package com.softwaremagico.tm.advisor.ui.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.softwaremagico.tm.advisor.R;
+
+public class Counter extends Component {
+    private static final int DURATION = 500;
+    private TextView tagText;
+    private ImageView gearImage;
+    private TextView valueText;
+    private int currentValue = 0;
+
+    public Counter(Context context) {
+        this(context, null);
+    }
+
+    public Counter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void init(Context context, AttributeSet attrs) {
+        inflate(context, R.layout.counter, this);
+        initComponents(attrs);
+    }
+
+    private void initComponents(AttributeSet attrs) {
+        tagText = findViewById(R.id.tag);
+        gearImage = findViewById(R.id.gear);
+        valueText = findViewById(R.id.value);
+    }
+
+    public void setValue(int value) {
+        valueText = findViewById(R.id.value);
+        valueText.setText(value + "");
+        if (currentValue < value) {
+            for (int i = 0; i < value - currentValue; i++) {
+                rotate(45f);
+            }
+        } else {
+            for (int i = 0; i < currentValue - value; i++) {
+                rotate(-45f);
+            }
+        }
+        currentValue = value;
+    }
+
+    public void setTag(String tag) {
+        tagText = findViewById(R.id.tag);
+        tagText.setText(tag);
+    }
+
+    private synchronized void rotate(float angle) {
+        final Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(DURATION);
+        gearImage.startAnimation(animation);
+    }
+
+
+}
