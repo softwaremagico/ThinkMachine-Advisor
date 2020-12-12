7fca329b25660b25a22db835620d21aebd870204
Software Magico <softwaremagico@gmail.com>
2020-12-12 08:26:18 +0100

    Lazy loading of fragments

--- app/src/main/java/com/softwaremagico/tm/advisor/ui/character/TabCharacterCreationFragment.java
+++ app/src/main/java/com/softwaremagico/tm/advisor/ui/character/TabCharacterCreationFragment.java
@@ -38,7 +38,8 @@ public class TabCharacterCreationFragment extends Fragment {
         viewPager.setAdapter(characterSectionsPagerAdapter);
 
         //Avoid refreshing of fragments. We will update them manually.
-        viewPager.setOffscreenPageLimit(characterSectionsPagerAdapter.getCount());
+        //viewPager.setOffscreenPageLimit(characterSectionsPagerAdapter.getCount());
+        viewPager.setOffscreenPageLimit(1);
 
         final TabLayout tabs = view.findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
