2476f96408f08fa5575b86d97abdeb0f86385d07
Software Mágico <softwaremagico@gmail.com>
2021-03-04 16:28:17 +0100

    Fixed deleting of searchable items

--- app/src/main/java/com/softwaremagico/tm/advisor/ui/components/IncrementalElementsLayout.java
+++ app/src/main/java/com/softwaremagico/tm/advisor/ui/components/IncrementalElementsLayout.java
@@ -66,17 +66,29 @@ public abstract class IncrementalElementsLayout<T extends Element<?>> extends Li
             addElementSpinner(createElementSpinner());
             return;
         }
-        for(int i=elementSpinners.size()-1; i>=0; i--){
+        boolean previousIsNull = false;
+        for (int i = elementSpinners.size() - 1; i >= 0; i--) {
             //Not the last spinner.
             if (i < elementSpinners.size() - 1) {
                 if (elementSpinners.get(i).getSelection() == null) {
-                    removeView(elementSpinners.get(i));
-                    elementSpinners.remove(i);
+                    if (previousIsNull) {
+                        removeView(elementSpinners.get(i + 1));
+                        elementSpinners.remove(i + 1);
+                    } else {
+                        removeView(elementSpinners.get(i));
+                        elementSpinners.remove(i);
+                    }
+                    previousIsNull = true;
+                } else {
+                    previousIsNull = false;
                 }
             } else {
                 //Last must be an empty one.
                 if (nullAllowed && elementSpinners.get(i).getSelection() != null) {
                     addElementSpinner(createElementSpinner());
+                    previousIsNull = false;
+                } else {
+                    previousIsNull = true;
                 }
             }
         }
