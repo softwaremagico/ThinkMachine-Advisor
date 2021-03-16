1a8c0cc6eb0dae1be989eb8889834c79d3a82c0f
Software Mágico <softwaremagico@gmail.com>
2021-03-16 18:03:04 +0100

    Added cybernetics traits to description

--- app/src/main/java/com/softwaremagico/tm/advisor/ui/components/descriptions/CyberneticDeviceDescriptionDialog.java
+++ app/src/main/java/com/softwaremagico/tm/advisor/ui/components/descriptions/CyberneticDeviceDescriptionDialog.java
@@ -5,6 +5,7 @@ import com.softwaremagico.tm.advisor.ui.session.CharacterManager;
 import com.softwaremagico.tm.advisor.ui.translation.ThinkMachineTranslator;
 import com.softwaremagico.tm.character.characteristics.CharacteristicName;
 import com.softwaremagico.tm.character.cybernetics.CyberneticDevice;
+import com.softwaremagico.tm.character.cybernetics.CyberneticDeviceTrait;
 import com.softwaremagico.tm.character.equipment.weapons.WeaponDamage;
 
 public class CyberneticDeviceDescriptionDialog extends ElementDescriptionDialog<CyberneticDevice> {
@@ -31,49 +32,51 @@ public class CyberneticDeviceDescriptionDialog extends ElementDescriptionDialog<
                         "<th>" + ThinkMachineTranslator.getTranslatedText("weaponRate") + "</th>" +
                         "</tr>");
                 for (WeaponDamage weaponDamage : cyberneticDevice.getWeapon().getWeaponDamages()) {
-                    boolean techDamageLimited = weaponDamage.getDamageTechLevel() != null &&
-                            CharacterManager.getSelectedCharacter().getCharacteristic(CharacteristicName.TECH).getValue() <
-                                    weaponDamage.getDamageTechLevel();
                     stringBuilder.append("<tr>");
-                    stringBuilder.append(
-                            "<td style=\"text-align:center\">" + weaponDamage.getGoal() + "</td>" +
-                                    "<td style=\"text-align:center\" >" + getDamage(weaponDamage) + "</td>" +
-                                    "<td style=\"text-align:center\" >" + weaponDamage.getRange() + "</td>" +
-                                    "<td style=\"text-align:center\" >" + (weaponDamage.getShots() != null ? weaponDamage.getShots() : "") + "</td>" +
-                                    "<td style=\"text-align:center\" >" + weaponDamage.getRate() + "</td>" +
-                                    "</tr>");
+                    stringBuilder.append("<td style=\"text-align:center\">").append(weaponDamage.getGoal()).append("</td>").
+                            append("<td style=\"text-align:center\" >").append(getDamage(weaponDamage)).append("</td>").
+                            append("<td style=\"text-align:center\" >").append(weaponDamage.getRange()).append("</td>").
+                            append("<td style=\"text-align:center\" >").append(weaponDamage.getShots() != null ? weaponDamage.getShots() : "").append("</td>").
+                            append("<td style=\"text-align:center\" >").append(weaponDamage.getRate()).append("</td>").
+                            append("</tr>");
                 }
                 stringBuilder.append("</table>");
             } else {
                 stringBuilder.append("<table cellpadding=\"" + TABLE_PADDING + "\" style=\"" + TABLE_STYLE + "\">");
                 stringBuilder.append("<tr>");
-                stringBuilder.append("<th>" + ThinkMachineTranslator.getTranslatedText("weaponGoal") + "</th>" +
-                        "<th>" + ThinkMachineTranslator.getTranslatedText("weaponDamage") + "</th>" +
-                        "</tr>");
+                stringBuilder.append("<th>").append(ThinkMachineTranslator.getTranslatedText("weaponGoal")).append("</th>").
+                        append("<th>").append(ThinkMachineTranslator.getTranslatedText("weaponDamage")).append("</th>").append("</tr>");
                 for (WeaponDamage weaponDamage : cyberneticDevice.getWeapon().getWeaponDamages()) {
-                    boolean techDamageLimited = weaponDamage.getDamageTechLevel() != null &&
-                            CharacterManager.getSelectedCharacter().getCharacteristic(CharacteristicName.TECH).getValue() <
-                                    weaponDamage.getDamageTechLevel();
                     stringBuilder.append("<tr>");
-                    stringBuilder.append("<td style=\"text-align:center\">" + weaponDamage.getGoal() + "</td>" +
-                            "<td style=\"text-align:center\" >" + getDamage(weaponDamage) + "</td>" +
-                            "</tr>");
+                    stringBuilder.append("<td style=\"text-align:center\">").append(weaponDamage.getGoal()).append("</td>").
+                            append("<td style=\"text-align:center\" >").append(getDamage(weaponDamage)).append("</td>").
+                            append("</tr>");
                 }
                 stringBuilder.append("</table>");
             }
+            stringBuilder.append("<br>");
         }
 
-        stringBuilder.append("<b>" + ThinkMachineTranslator.getTranslatedText("techLevel") + ": " + "</b>" +
-                (techLimited ? "<font color=\"" + getColor(R.color.insufficientTechnology) + "\">" : "") +
-                cyberneticDevice.getTechLevel() +
-                (techLimited ? "</font>" : "") +
-                "<br><b>" + getString(R.string.incompatibility) + ": </b>" +
-                cyberneticDevice.getIncompatibility() +
-                "<br><b>" + getString(R.string.points) + ": </b>" +
-                cyberneticDevice.getPoints() +
-                "<br><b>" + getString(R.string.cost) + ": </b> " +
-                cyberneticDevice.getCost() +
-                " " + ThinkMachineTranslator.getTranslatedText("firebirds"));
+        //Set traits
+        if (!cyberneticDevice.getTraits().isEmpty()) {
+            stringBuilder.append("<b>").append(ThinkMachineTranslator.getTranslatedText("cyberneticsTraits")).append(": ").append("</b>");
+            String separator = "";
+            for (CyberneticDeviceTrait trait : cyberneticDevice.getTraits()) {
+                stringBuilder.append(separator);
+                stringBuilder.append(trait.getName());
+                separator = ", ";
+            }
+            stringBuilder.append("<br>");
+        }
+
+        //Set costs.
+        stringBuilder.append("<b>").append(ThinkMachineTranslator.getTranslatedText("techLevel")).append(": ").append("</b>").
+                append(techLimited ? "<font color=\"" + getColor(R.color.insufficientTechnology) + "\">" : "").
+                append(cyberneticDevice.getTechLevel()).append(techLimited ? "</font>" : "").append("<br><b>").
+                append(getString(R.string.incompatibility)).append(": </b>").append(cyberneticDevice.getIncompatibility()).append("<br><b>").
+                append(getString(R.string.points)).append(": </b>").append(cyberneticDevice.getPoints()).append("<br><b>").
+                append(getString(R.string.cost)).append(": </b> ").append(cyberneticDevice.getCost()).append(" ").
+                append(ThinkMachineTranslator.getTranslatedText("firebirds"));
 
         return stringBuilder.toString();
     }
